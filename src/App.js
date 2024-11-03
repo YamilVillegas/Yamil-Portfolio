c584db1a60ec23acbf6543ca52593ad5d66e6d65
Yamil Villegas <YTorres87@gmail.com>
2024-11-02 22:45:36 -0400

    About Me Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import ProjectsList from './features/projects/ProjectsList.js';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <AboutMe/>
       <ProjectsList/>
     </div>
   );
--- src/features/projects/ProjectsList.js
+++ src/features/projects/ProjectsList.js
@@ -8,7 +8,7 @@ const ProjectsList = () => {
             <div className="container">
                 <Row className="justify-content-center">
                     {PROJECTS.map((project) => (
-                        <Col xs={12} md={4} className="m-2" key={project.id}>
+                        <Col xs={12} md={4} className="m-" key={project.id}>
                             <ProjectsCard project={project} />
                         </Col>
                     ))}
