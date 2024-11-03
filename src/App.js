6830378ae65ac4e2fb412c821c79f85899a3b429
Yamil Villegas <YTorres87@gmail.com>
2024-11-03 13:46:17 -0500

    Hero Section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import ProjectsList from './features/projects/ProjectsList.js';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HeroSection/>
       <AboutMe/>
       <ProjectsList/>
     </div>
---
+++ src/components/HeroSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import headshot from '../app/assets/img/AppleImage.jpeg'
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h1 className='name-text'>Yamil.</h1>
+            <h2>Web Designer & Frontend Developer</h2>
+            <p>Dedicated to crafting exceptional digital solutions. Offering expertise in web design and frontend development.</p>
+            <p>Custom website design tailored to your brand, driving online success.</p>
+            <p>Responsive web development for seamless browsing across devices.</p>
+          </Col>
+          <Col md={6}>
+            <img src= {headshot} alt="Headshot" className="img-fluid rounded-circle" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
